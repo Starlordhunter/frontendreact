e7d94f683f931d330e40edc888e2f6605e7c2621
Zaki <87921831+Starlordhunter@users.noreply.github.com>
2022-09-14 10:39:22 +0800

    Update app.js
    using route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Login from './components/login';
 import Students from './components/students';
 import Teachers from './components/teachers';
+import Principal from './components/principal';
+import { Router,Route,Routes, Link} from 'react-router-dom';
+
 
 function App() {
 
@@ -16,9 +19,28 @@ function App() {
 
   return (
     <div className='App'>
-      <Login userLogin={userLogin}/>
+      {/* <Login userLogin={userLogin}/>
       <Teachers token={token}/>
       <Students token={token}/>
+      <Principal token={token}/> */}
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/login">Log in</Link> |{" "}
+        <Link to="/teachers">Teachers</Link> |{" "}
+        <Link to="/students">Students</Link> |{" "}
+        <Link to="/principal">Principal</Link>
+      </nav>
+
+      <Routes>
+        <Route path='/login' element={<Login userLogin={userLogin}/>}/>
+        <Route path='/teachers' element={<Teachers token={token}/>}/>
+        <Route path='/students' element={<Students token={token}/>}/>
+        <Route path='/principal' element={<Principal token={token}/>}/>
+      </Routes>
     </div>
   );
 }
